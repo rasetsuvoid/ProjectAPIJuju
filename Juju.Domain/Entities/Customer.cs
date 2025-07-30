1fbf02e0216e923b158e23857362d17db615657c
raset <raset@DESKTOP-2TGGUJA>
2025-07-30 18:34:58 -0500

    Añadir clases Customer y Post, actualizar proyectos
    Se han creado las clases `Customer` y `Post` en el espacio de nombres `Juju.Domain.Entities`, con propiedades relevantes. Se han actualizado los archivos de proyecto `Juju.Domain.csproj` y `DataAccess.csproj` para incluir las configuraciones necesarias para .NET 9.0. Además, se ha modificado el archivo de solución `ProjectAPI.sln` para incluir el nuevo proyecto `Juju.Domain`.

--- Juju.Domain/Entities/Customer.cs
+++ Juju.Domain/Entities/Customer.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Juju.Domain.Data
+namespace Juju.Domain.Entities
 {
-    public partial class Customer
+    public class Customer
     {
         public int CustomerId { get; set; }
         public string Name { get; set; }
     }
+
 }
---
+++ Juju.Domain/Entities/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juju.Domain.Entities
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public int Type { get; set; }
+        public string Category { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
