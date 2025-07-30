71f1c163bb6c380abd0d777c0c0335bed0c5dda2
raset <raset@DESKTOP-2TGGUJA>
2025-07-30 18:59:29 -0500

    Refactor y mejora de la estructura del repositorio
    Se ha añadido una referencia de proyecto a `Juju.Application` en `Juju.Infrastructure.csproj`, reemplazando la referencia anterior a `Juju.Domain`. Además, se ha añadido un grupo de elementos en `Juju.Application.csproj` que incluye una referencia a `Juju.Domain`.
    
    En `Repository.cs`, la clase `Repository` se ha hecho pública y se han implementado métodos asíncronos para operaciones CRUD, junto con bloques de manejo de excepciones. También se ha creado una nueva interfaz `IRepository` en `IRepository.cs` que define los métodos necesarios para cualquier repositorio.

--- Juju.Infrastructure/Repository/Repository.cs
+++ Juju.Infrastructure/Repository/Repository.cs
@@ -1,12 +1,142 @@
-﻿using System;
+﻿using DataAccess.Data;
+using Juju.Application.Contracts;
+using Juju.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Juju.Infrastructure.Repository
 {
-    internal class Repository
+    public class Repository<T> : IRepository<T> where T : BaseEntity
     {
+        protected readonly JujuTestContext _context;
+        public Repository(JujuTestContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync(params Expression<Func<T, bool>>[] includes)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync(Expression<Func<T, bool>> filter = null, params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(long id)
+        {
+            return await _context.Set<T>().FindAsync(id)!;
+        }
+
+        public async Task<IReadOnlyList<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+
+            try
+            {
+                await _context.Set<T>().AddAsync(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task AddRangeAsync(IReadOnlyList<T> entities)
+        {
+            await _context.Set<T>().AddRangeAsync(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public async Task<T> GetByIdAsync(Expression<Func<T, bool>> predicate)
+        {
+            try
+            {
+                return await _context.Set<T>()?.Where(predicate)?.FirstOrDefaultAsync()!;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<T> GetByIdWithIncludeAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes)
+        {
+            try
+            {
+                var query = _context.Set<T>().Where(predicate);
+
+                // Agregar includes a la consulta
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+
+                return await query.FirstOrDefaultAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task UpdatePartialAsync(T entity, params Expression<Func<T, object>>[] updatedProperties)
+        {
+            _context.Set<T>().Attach(entity);
+            entity.UpdatedDate = DateTime.Now;
+
+            foreach (var property in updatedProperties)
+            {
+                _context.Entry(entity).Property(property).IsModified = true;
+            }
+        }
     }
 }
