844dfd9fed63134e190c55648a2e1bb8019c2232
raset <raset@DESKTOP-2TGGUJA>
2025-07-30 18:13:36 -0500

    Eliminada clase innecesaria y añadida infraestructura DI
    Se eliminó la clase `Class1` del archivo `Class1.cs`, que no contenía ninguna implementación. Se actualizó el archivo de proyecto `Juju.Infrastructure.csproj` para incluir una nueva referencia de paquete a `Microsoft.Extensions.DependencyInjection.Abstractions` en la versión `9.0.7`. Además, se agregó una nueva clase estática `ConfigurationServices` en el archivo `ConfigurationServices.cs`, que incluye un método de extensión `AddInfrastructureServices` para `IServiceCollection`, preparando así la infraestructura para registrar servicios en el contenedor de inyección de dependencias.

---
+++ Juju.Infrastructure/ConfigurationServices.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Juju.Infrastructure
+{
+    public static class ConfigurationServices
+    {
+        public static void AddInfrastructureServices(this IServiceCollection services)
+        {
+
+        }
+    }
+}
